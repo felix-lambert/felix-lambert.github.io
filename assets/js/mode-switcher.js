fa19b31e3e4dfe8dbeb32ac0c045268e86b99d13
felix <lambertfelix8@gmail.com>
2021-03-14 13:54:47 +0100

    major updated

--- assets/js/mode-switcher.js
+++ assets/js/mode-switcher.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", themeChange);
 
+const theme = localStorage.getItem("theme");
+
+console.log("theme");
+console.log(theme);
+
+if (theme === "dark") {
+  document.documentElement.setAttribute("data-theme", "dark");
+}
+if (theme === "light") {
+  document.documentElement.setAttribute("data-theme", "light");
+}
+
 function themeChange() {
   // Select our toggle button
   let button = document.querySelector(".theme-toggle");
@@ -9,10 +21,14 @@ function themeChange() {
     // Check the current data-theme value
     let currentTheme = document.documentElement.getAttribute("data-theme");
 
+    console.log(theme);
+
     if (currentTheme === "dark") {
       document.documentElement.setAttribute("data-theme", "light");
+      window.localStorage.setItem("theme", "light");
     } else {
       document.documentElement.setAttribute("data-theme", "dark");
+      window.localStorage.setItem("theme", "dark");
     }
   });
 }
